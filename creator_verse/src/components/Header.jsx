8bbed647740dd2663ede511077a556df15df7580
Leigh-Riane Amsterdam <amsterdam.l@northeastern.edu>
2025-08-17 20:23:36 -0400

    adding header and style to prepare for routes

---
+++ creator_verse/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header-class">
+            <h1>Welcome to Creatorverse</h1>
+        </div>
+    );
+};
+export default Header
