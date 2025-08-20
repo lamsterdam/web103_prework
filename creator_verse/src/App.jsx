fcd4092ae41c7c32205d2e3c88989e8da82d18a6
Leigh-Riane Amsterdam <amsterdam.l@northeastern.edu>
2025-08-20 14:30:12 -0400

    set up routes and wrapper

--- creator_verse/src/App.jsx
+++ creator_verse/src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
+import { Children, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './components/Header'
+import { useRoutes } from 'react-router-dom'
+import Layout from './components/Layout'
+import ShowCreators from './pages/ShowCreators'
+import AddCreator from './pages/AddCreator'
+import EditCreator from './pages/EditCreator'
+import ViewCreator from './pages/ViewCreator'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const routes = useRoutes([
+    {path: "/", element: <Layout />,
+      children: [
+        {index: true, element: <ShowCreators />},
+        {path: "/show", element: <ShowCreators />},
+        {path: "/add", element: <AddCreator />},
+        {path: "/edit", element: <EditCreator />},
+        {path: "/view", element: <ViewCreator />}
+      ]
+    }
+  ]);
+  
 
   return (
     <>
-      <Header />
+      {routes}
     </>
   )
 }
