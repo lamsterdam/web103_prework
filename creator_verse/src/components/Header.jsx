5f9dcda1f07203c213bbac8ba63ccb82522bf6ed
Leigh-Riane Amsterdam <amsterdam.l@northeastern.edu>
2025-08-20 16:59:56 -0400

    added hooks for viewing single creator

--- creator_verse/src/components/Header.jsx
+++ creator_verse/src/components/Header.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 const Header = () => {
+    const navigate = useNavigate();
+    const handleChange = () => {
+        navigate("/add");
+    }
+
     return (
         <div className="header-class">
             <h1>Welcome to Creatorverse</h1>
+            <button onChange={handleChange}>Add a Creator</button>
         </div>
     );
 };
--- creator_verse/src/pages/ViewCreator.jsx
+++ creator_verse/src/pages/ViewCreator.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { supabase } from "../client";
+import { useParams } from "react-router-dom"; 
 
 // Page to view details of a single content creator
 const ViewCreator = () => {
+    const [contentCreator, setContentCreator] = useState(null);
+    const {id} = useParams();
+    useEffect (() => {
+        const fetchCreator = async() => {
+            const {data} = await supabase
+            .from("creators")
+            .select()
+            .eq("id", id)
+            .single()
+
+            setContentCreator(data)
+        }
+        fetchCreator();
+
+    }, [id]);
     return (
         <div>
-
+            {contentCreator && (
+                <div>
+                    <div>Name: {contentCreator.name}</div>
+                    <div>Description: {contentCreator.description}</div>
+                    <div>URL: {contentCreator.url}</div>
+                </div>)}
         </div>
     );
 };
